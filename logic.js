9ab443c34da1c1d8e408b1b9330c126fcd7c59cc
Francisco <65142449+DaFrancc@users.noreply.github.com>
2022-09-13 13:58:12 -0500

    Basic website
    Made a quick website

---
+++ logic.js
@@ -0,0 +1,48 @@
+var isUp = false;
+function toggleTab() {
+    let nav = document.getElementById("nav-tab");
+    let tb = document.getElementById("top-bar");
+    if (window.matchMedia("(orientation: portrait)").matches) {
+        if(!isUp){
+            nav.style.transform = "translateY(-500%)";
+            tb.style.transform = "translateY(-100%)";
+            document.getElementById("toggleTab").innerHTML = "Show";
+            isUp = true;
+        } else {
+            nav.style.transform = "translateY(-250%)";
+            tb.style.transform = "translateY(0%)";
+            document.getElementById("toggleTab").innerHTML = "Hide";
+            isUp = false;
+        }
+    } else if (window.matchMedia("(orientation: landscape)").matches) {
+        if(!isUp){
+            nav.style.transform = "translateY(-300%)";
+            tb.style.transform = "translateY(-100%)";
+            document.getElementById("toggleTab").innerHTML = "Show";
+            isUp = true;
+        } else {    
+            nav.style.transform = "translateY(-100%)";
+            tb.style.transform = "translateY(0%)";
+            document.getElementById("toggleTab").innerHTML = "Hide";
+            isUp = false;
+        }
+    }
+}
+
+function toggleTab2() {
+    let nav = document.getElementById("nav-tab");
+    let tb = document.getElementById("top-bar");
+    if(window.scrollY == 0) {
+        nav.style.transform = "translateY(-125%)";
+        tb.style.transform = "translateY(0%)";
+        isUp = false;
+    }
+    else {
+        nav.style.transform = "translateY(-250%)";
+        tb.style.transform = "translateY(-100%)";
+        isUp = true;
+    }
+}
+
+window.DeviceOrientationEvent = () => {toggleTab();}
+window.addEventListener("scroll", (e) => {toggleTab2();});
