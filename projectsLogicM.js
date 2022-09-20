a5091c0e3fe0b62627aeb2553995349b16a3acae
Francisco <xenonaught@gmail.com>
2022-09-20 11:46:14 -0500

    Added projects tab and optimized for mobile

--- projectsLogicM.js
+++ projectsLogicM.js
@@ -9,14 +9,14 @@ function toggleTab() {
     let tb = document.getElementById("top-bar");
     let tbl = document.getElementById("topbreakline");
     if(window.scrollY == 0) {
-        nav.style.transform = "translateY(-125%)";
+        nav.style.transform = "translateY(-75%)";
         tb.style.transform = "translateY(0%)";
         tb.style.boxShadow = "0px 5px 1em #000000aa";
         tbl.style.height = "7.5em";
         isUp = false;
     }
     else {
-        nav.style.transform = "translateY(-250%)";
+        nav.style.transform = "translateY(-275%)";
         tb.style.transform = "translateY(-100%)";
         tbl.style.height = "0";        
         tb.style.boxShadow = "0px 0px 0em #000000aa";
